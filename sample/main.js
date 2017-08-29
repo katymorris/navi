cc5100ad1105ac44132246a2671f41783e7bc14e
Katy Morris <katymcconica@gmail.com>
2017-08-29 13:12:12 -0700

    added header, wrapped in anon function, made adjustments to main var

--- sample/main.js
+++ sample/main.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
 	goNavi({
-		menuTitle: "Menu",
-		color: "gray",
-		mobileWindowWidth: 768,
-		navWidth: "50%",
-		panelPosition: "right"
+		menuTitle: "Your Menu",
+		color: "blue",
+		mobileWindowWidth: 980,
+		navWidth: "60%",
+		// panelPosition: "right"
 	});
 });
 
--- sample/navi.js
+++ sample/navi.js
@@ -1,73 +1,123 @@
-/*-----------------GLOBALS-----------------*/
-var userProps;
+/*!
+ * Navi - Navigation Panel
+ * (c) 2017 Katy D M Morris
+ * 
+ *
+ * http://katy-morris.com/navi
+ * http://github.com/katy-morris/navi
+ *
+ * VERSION 1.0
+ */
 
+(function (window) {
 
-/*----------------GENERAL FUNCTIONS----------------*/
+  /*-----------------DEFAULT PROPS------------------*/
 
-//check for class
-function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-}
-
-/*-----------------NAVI FUNCTIONS------------------*/
-
-function goNavi(props) {
-  //set prop global variable
-  userProps = props;
-
-  //set up starting variables
-  var navigation = document.getElementById("navi");
-
-  buildNavigationBasis(navigation);
-
-
-  checkForActivation();
-}
-
-function buildNavigationBasis(navigation) {
-
-  //create menu title
-  var navFirstChild = navigation.children[0];
-  var menuTitle = document.createElement('div');
-  menuTitle.setAttribute("id", "navi-menu-title");
-  menuTitle.innerHTML = userProps.menuTitle;
-  navigation.prepend(menuTitle);
-
-  //set nav position
-  if (userProps.panelPosition == "right") {
-    navigation.className += " right-panel";
-  } else if (userProps.panelPosition == "left") {
-    navigation.className += " left-panel";
+  //default properties
+  var userProps = {
+      navbarEl: null,
+      menuTitle: "Menu",
+      color: "gray",
+      mobileWindowWidth: 768,
+      navWidth: "50%",
+      panelPosition: "left"
   }
 
-  setMobileAttributes(navigation);
+  /*----------------GENERAL FUNCTIONS----------------*/
 
-}
-//set styles on mobile
-function setMobileAttributes(navigation) {
-  if (window.outerWidth < userProps.mobileWindowWidth) {
-    navigation.style.width = userProps.navWidth;
-  } else {
-    navigation.removeAttribute('style');
+  //check for class
+  function hasClass(element, cls) {
+      return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
   }
-}
-//check at end of preparation or resize if navigation should show
-function checkForActivation() {
-  var windowWidth = window.outerWidth;
-  var navigation = document.getElementById("navi");
-  var activated = hasClass(navigation, 'nav-activated');
 
-  if (windowWidth < userProps.mobileWindowWidth && activated == false) {
-    navigation.className += " nav-activated";
-  } else if (activated == true && windowWidth > userProps.mobileWindowWidth) {
-    navigation.className = navigation.className.replace(new RegExp('(?:^|\\s)'+ 'nav-activated' + '(?:\\s|$)'), ' ');
-  }
-}
+  /*-----------------NAVI FUNCTIONS------------------*/
 
-document.addEventListener("DOMContentLoaded", function(){
 
-});
+  /*----MAIN PIPELINE----*/
 
-window.onresize = function(event) {
+  function goNavi(props) {
+
+    //set custom properties
+    var x;
+    for (x in props) {
+      userProps[x] = props[x];
+    }
+
+    //get user's navbar
+    userProps.navbarEl = document.getElementById("navi");
+
+    buildNavigationBase();
+
+
     checkForActivation();
-};
+  }
+  window.goNavi = goNavi;
+
+  /*-----------------FACTORY------------------*/
+
+//runs once on document creation
+  function buildNavigationBase() {
+
+    //create menu title
+    var navFirstChild = userProps.navbarEl.children[0];
+    var menuTitle = document.createElement('div');
+    menuTitle.setAttribute("id", "navi-menu-title");
+    menuTitle.innerHTML = userProps.menuTitle;
+    userProps.navbarEl.prepend(menuTitle);
+
+    //sort out the user's input for nav width for use
+    var navWidthInt = parseInt(userProps.navWidth) * -1;
+    var navWidthType = userProps.navWidth.match(/\D/g,'').join("");
+    var finalNavWidthVal = navWidthInt + navWidthType;
+
+    //set nav position
+    if (userProps.panelPosition == "right") {
+      userProps.navbarEl.className += " right-panel";
+      userProps.navbarEl.style.right = finalNavWidthVal
+    } else if (userProps.panelPosition == "left") {
+      userProps.navbarEl.className += " left-panel";
+      userProps.navbarEl.style.left = finalNavWidthVal
+    }
+
+  setMobileAttributes();
+
+  }
+
+  //runs on resize
+  //set styles on mobile
+  function setMobileAttributes() {
+    if (window.outerWidth < userProps.mobileWindowWidth) {
+      userProps.navbarEl.style.width = userProps.navWidth;
+    } else {
+      userProps.navbarEl.removeAttribute('style');
+    }
+  }
+
+  /*-----------------RESIZE EVENTS------------------*/
+
+  //check at end of preparation or resize if navigation should show
+  function checkForActivation() {
+    var windowWidth = window.outerWidth;
+    var activated = hasClass(userProps.navbarEl, 'nav-activated');
+
+    if (windowWidth < userProps.mobileWindowWidth && activated == false) {
+      userProps.navbarEl.className += " nav-activated";
+    } else if (activated == true && windowWidth > userProps.mobileWindowWidth) {
+      userProps.navbarEl.className = userProps.navbarEl.className.replace(new RegExp('(?:^|\\s)'+ 'nav-activated' + '(?:\\s|$)'), ' ');
+    }
+  }
+
+
+  /*-----------------DOM LOADED------------------*/
+  /*---------------------------------------------*/
+  document.addEventListener("DOMContentLoaded", function(){
+
+  });
+
+
+  /*-----------------RESIZE------------------*/
+  /*-----------------------------------------*/
+  window.onresize = function(event) {
+      checkForActivation();
+  };
+} (window))
